8e936d76519ea35ee636d538785fc57db1be6c80
Sejin Jeon <jinaidy93@gmail.com>
2018-07-20 18:28:35 +0900

    Add a function init2WdVector to vector_util.h

--- vector_util.h
+++ vector_util.h
@@ -15,4 +15,16 @@ void printVector(vector<T> v, string separator = "\n") {
     cout << endl;
 }
 
+template<typename T>
+vector<vector<T>> init2WdVector(vector<vector<T>> v, int w, int h, T initElem = 0) {
+    for(int i=0; i<h; i++) {
+        vector<T> tmp;
+        for(int j=0; j<w; j++) {
+            tmp.emplace_back(initElem);
+        }
+        v.emplace_back(tmp);
+    }
+    return v;
+}
+
 #endif //ALGORITHM_HUSTLE_VECTOR_UTIL_H
