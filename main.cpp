1d9a4f1d0451ef9a39ba2a09b3761dd6c308627e
Sejin Jeon <jinaidy93@gmail.com>
2018-07-25 11:39:11 +0900

    Create pr12951.cpp - generating a JadenCase string

--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <vector>
 #include "vector_util.h"
-#include "pr12949.cpp"
+#include "pr12951.cpp"
 
 using namespace std;
 
 int main() {
-    vector<vector<vector<int>>> testCases = {
-            {{1, 4}, {3, 2}, {4, 1}},
-            {{2, 3, 2}, {4, 2, 4}, {3, 1, 4}}
+    vector<string> testCases = {
+            "3people unFollowed me",
+            "for the last week"
     };
-    vector<vector<vector<int>>> testCases2 = {
-            {{3, 3}, {3, 3}},
-            {{5, 4, 3}, {2, 4, 1}, {3, 1, 1}}
-    };
+//    vector<vector<vector<int>>> testCases2 = {
+//            {{3, 3}, {3, 3}},
+//            {{5, 4, 3}, {2, 4, 1}, {3, 1, 1}}
+//    };
     int numCases = testCases.size();
     for (int i = 0; i < numCases; i++) {
 //        cout << (solution(testCases[i]) ? "true" : "false") << endl;
-//        cout << solution(testCases[i]) << endl;
+        cout << solution(testCases[i]) << endl;
 //        cout << solution(testCases[i], testCases2[i]) << endl;
-        vector<vector<int>> result = solution(testCases[i], testCases2[i]);
-        for(int i=0; i<result.size(); i++)
-            printVector(result[i], " ");
+//        vector<vector<int>> result = solution(testCases[i], testCases2[i]);
+//        for(int i=0; i<result.size(); i++)
+//            printVector(result[i], " ");
     }
     return 0;
 }
---
+++ pr12951.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include "string_util.h"
+
+using namespace std;
+
+string toLower(string s) {
+    string::iterator iter;
+    for(iter = s.begin(); iter != s.end(); iter++) {
+        if(*iter >= 'A' && *iter <= 'Z')
+            *iter += 'a' - 'A';
+    }
+    return s;
+}
+
+string solution(string s) {
+    string answer = "";
+    s = toLower(s);
+    vector<string> fragments = split(s, " ");
+    vector<string>::iterator iter;
+    for(iter = fragments.begin(); iter != fragments.end(); iter++) {
+        if((*iter)[0] >= 'a' && (*iter)[0] <= 'z')
+            (*iter)[0] += 'A' - 'a';
+        answer += *iter;
+        if(iter + 1 == fragments.end()) break;
+        answer += ' ';
+    }
+    return answer;
+}
