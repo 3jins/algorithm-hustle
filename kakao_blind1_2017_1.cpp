4b73bde79dbfa4f677acac8a1e1f7934c99f8ebe
Sejin Jeon <jinaidy93@gmail.com>
2018-09-12 16:52:21 +0900

    Modify kakao_blind1_2017_1.cpp

--- kakao_blind1_2017_1.cpp
+++ kakao_blind1_2017_1.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-string convertMap(int deca) {
+string convertMap(int deca, int mapSize) {
     string mapRow = "";
     while(deca > 0) {
         if(deca % 2 == 1) {
@@ -14,6 +14,10 @@ string convertMap(int deca) {
         }
         deca /= 2;
     }
+    int diff = mapSize - mapRow.length();
+    for(int i=0; i<diff; i++) {
+        mapRow = " " + mapRow;
+    }
     return mapRow;
 }
 
@@ -22,7 +26,7 @@ vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     int arrSize = arr1.size();
 
     for (int i = 0; i < arrSize; i++) {
-        map.emplace_back(convertMap(arr1[i] | arr2[i]));
+        map.emplace_back(convertMap(arr1[i] | arr2[i], n));
     }
     return map;
 }
--- main.cpp
+++ main.cpp
@@ -1,76 +1,67 @@
 #include <iostream>
 #include <vector>
 #include "vector_util.h"
-#include "bj14499_samsung.cpp"
+#include "kakao_blind1_2017_1.cpp"
 
 using namespace std;
 
 int main() {
     /* Baekjoon Style Main */
-    int N, M;
-    int samsungY, samsungX;
-    int K;
-    vector<vector<int>> map;
-    vector<int> commands;
-    cin >> N >> M >> samsungX >> samsungY >> K;
-    for(int i=0; i<N; i++) {
-        vector<int> row;
-        for(int j=0; j<M; j++) {
-            int coordVal;
-            cin >> coordVal;
-            row.push_back(coordVal);
-        }
-        map.push_back(row);
-    }
-    for(int i=0; i<K; i++) {
-        int command;
-        cin >> command;
-        commands.push_back(command);
-    }
-
-    solution(samsungX, samsungY, map, commands, N, M);
+//    int N, M;
+//    int samsungY, samsungX;
+//    int K;
+//    vector<vector<int>> map;
+//    vector<int> commands;
+//    cin >> N >> M >> samsungX >> samsungY >> K;
+//    for(int i=0; i<N; i++) {
+//        vector<int> row;
+//        for(int j=0; j<M; j++) {
+//            int coordVal;
+//            cin >> coordVal;
+//            row.push_back(coordVal);
+//        }
+//        map.push_back(row);
+//    }
+//    for(int i=0; i<K; i++) {
+//        int command;
+//        cin >> command;
+//        commands.push_back(command);
+//    }
+//
+//    solution(samsungX, samsungY, map, commands, N, M);
 
     /* Programmers Style Main */
-//    vector<vector<string>> testCases = {
-//            {"go","gone","guild"},
-//            {"abc","def","ghi","jklm"},
-//            {"word","war","warrior","world"}
-//    };
-//    vector<vector<string>> testCases2 = {
-//            {"12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"},
-//            {"03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"},
-//            {"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"},
-//    };
-//    vector<int> testCases3 = {
-//            2, 2, 2
-//    };
-//    vector<int> testCases4 = {
-//            1, 1, 2
-//    };
-
-//    int numCases = testCases.size();
+    vector<int> testCases = {
+            5, 6
+    };
+    vector<vector<int>> testCases2 = {
+            {9,  20, 28, 18, 11},
+            {46, 33, 33, 22, 31, 50}
+    };
+    vector<vector<int>> testCases3 = {
+            {30, 1,  21, 17, 28},
+            {27, 56, 19, 14, 14, 10}
+    };
+    int numCases = testCases.size();
 //    for (int i = 0; i < numCases; i++) {
+    for (int i = 1; i < 2; i++) {
 //        cout << (solution(testCases[i]) ? "true" : "false") << endl;
 //        cout << testCases[i] << endl;
 //        cout << solution(testCases[i]) << endl;
 //        cout << solution(testCases[i], testCases2[i]) << endl;
 //        cout << solution(testCases[i], testCases2[i], testCases3[i]) << endl;
-//        cout << solution(testCases[i], testCases2[i], testCases3[i], testCases4[i]) << endl;
-//        cout << solution(testCases[i], testCases2[i]) << endl;
 
-//        solution(testCases[i]);
-//        vector<string> result = solution(testCases[i]);
-//        vector<string> result = solution(testCases[i], testCases2[i]);
-//        vector<string> result = solution(testCases[i], testCases2[i], testCases3[i]);
+        vector<string> result = solution(testCases[i], testCases2[i], testCases3[i]);
 //        for(int i=1; i<=result.size(); i++) {
 //            cout << i << "\t";
 //        }
-//        printVector(result, ", ");
-
+//        cout << endl;
+        printVector(result, "\n");
+        cout << endl;
 //        vector<vector<int>> result = solution(testCases[i]);
 //        for(int i=0; i<result.size(); i++)
 //            printVector(result[i], " ");
-//    }
+    }
 
 
     return 0;
