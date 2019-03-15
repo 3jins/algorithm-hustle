ed998efda232cca7ad0b09c55c48d595ba44876d
Sejin Jeon <jinaidy93@gmail.com>
2019-03-15 21:56:27 +0900

    Create Stock11501.java

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,9 +1,16 @@
 import baekjoon.Fibonacci2747;
+import baekjoon.Stock11501;
+import baekjoon.SuffixArray9248;
 import baekjoon.koi.elmentary.DrawingArrows15970;
 import baekjoon.koi.elmentary.Happiness15969;
+import baekjoon.koi.elmentary.TwoRobots15971;
+import util.Sort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String args[]) {
-        new DrawingArrows15970();
+        new Stock11501();
     }
 }
---
+++ java/src/baekjoon/Stock11501.java
@@ -0,0 +1,55 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+// 20:41 ~
+public class Stock11501 {
+    private int mNumTestCases;
+    private ArrayList<Integer>[] mStockValueList;
+    private int[] mDays;
+
+    public Stock11501() {
+        init();
+        for (int i = 0; i < mNumTestCases; i++) {
+            System.out.println(getMaxBenefit(i));
+        }
+    }
+
+    public void init() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            mNumTestCases = Integer.parseInt(reader.readLine());
+            mStockValueList = new ArrayList[mNumTestCases];
+            mDays = new int[mNumTestCases];
+            for (int i = 0; i < mNumTestCases; i++) {
+                mStockValueList[i] = new ArrayList<>();
+                mDays[i] = Integer.parseInt(reader.readLine());
+                String stockValueString = reader.readLine();
+                StringTokenizer tokenizer = new StringTokenizer(stockValueString);
+                for (int j = 0; j < mDays[i]; j++) {
+                    mStockValueList[i].add(Integer.parseInt(tokenizer.nextToken()));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public long getMaxBenefit(int testCase) {
+        long maxBenefit = 0;
+        int localMaxValue = mStockValueList[testCase].get(mDays[testCase] - 1);
+        for (int i = mDays[testCase] - 2; i >= 0; i--) {
+            if (mStockValueList[testCase].get(i) < localMaxValue) {
+                maxBenefit += (localMaxValue - mStockValueList[testCase].get(i));
+            } else {
+                localMaxValue = mStockValueList[testCase].get(i);
+            }
+        }
+
+        return maxBenefit;
+    }
+}
