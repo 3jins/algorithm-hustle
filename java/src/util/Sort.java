33bcadc42a107f959b7a75664f947b09ed1903ac
Sejin Jeon <jinaidy93@gmail.com>
2019-03-15 20:03:22 +0900

    Add mergesort to Sort class

--- java/src/util/Sort.java
+++ java/src/util/Sort.java
@@ -9,13 +9,14 @@ public class Sort {
         this.mList = list;
     }
 
+    /* quick sort */
     private void quickSortConcat(int leftIdx, ArrayList<Integer> leftList, ArrayList<Integer> rightList) {
         int leftListSize = leftList.size();
         int rightListSize = rightList.size();
         for (int i = 0; i < leftListSize; i++) {
             mList.set(leftIdx + i, leftList.get(i));
         }
-        for(int i = 0; i < rightListSize; i++) {
+        for (int i = 0; i < rightListSize; i++) {
             mList.set(leftIdx + leftList.size() + i, rightList.get(i));
         }
     }
@@ -40,6 +41,46 @@ public class Sort {
 
     public ArrayList<Integer> quickSort() {
         quickSort(0, mList.size() - 1);
+        return mList;
+    }
+
+    /* merge sort */
+    private void mergeSort(int leftIdx, int rightIdx) {
+        if (leftIdx == rightIdx) return;
+        int leftIdx1 = leftIdx;
+        int rightIdx1 = (leftIdx + rightIdx) / 2;
+        int leftIdx2 = (leftIdx + rightIdx) / 2 + 1;
+        int rightIdx2 = rightIdx;
+        mergeSort(leftIdx1, rightIdx1);
+        mergeSort(leftIdx2, rightIdx2);
+
+        ArrayList<Integer> sortedList = new ArrayList<Integer>(rightIdx - leftIdx + 1);
+        int i = leftIdx1, j = leftIdx2;
+        while (i <= rightIdx1 && j <= rightIdx2) {
+            if (mList.get(i) < mList.get(j)) {
+                sortedList.add(mList.get(i));
+                i++;
+            } else {
+                sortedList.add(mList.get(j));
+                j++;
+            }
+        }
+        while (i <= rightIdx1) {
+            sortedList.add(mList.get(i));
+            i++;
+        }
+        while (j <= rightIdx2) {
+            sortedList.add(mList.get(j));
+            j++;
+        }
+
+        for (int k = leftIdx; k <= rightIdx; k++) {
+            mList.set(k, sortedList.get(k - leftIdx));
+        }
+    }
+
+    public ArrayList<Integer> mergeSort() {
+        mergeSort(0, mList.size() - 1);
         return mList;
     }
 }
